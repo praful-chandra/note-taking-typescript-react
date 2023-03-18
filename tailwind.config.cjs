a86e3444519c6f937535d99bedff7ac4d63b6ca0
praful chandra <prafulchandra@prafuls-MacBook-Pro.local>
2023-03-18 23:25:09 +0530

    added eslint and other dev tools

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: "#FCA311",
-        secondary: "#14213D",
-        backgroundBase: "#F0F0F0",
+        primary: '#FCA311',
+        secondary: '#14213D',
+        backgroundBase: '#F0F0F0',
         grey: {
-          regular: "#C9C9C9",
-          dark: "#A5A5A5",
-        },
+          regular: '#C9C9C9',
+          dark: '#A5A5A5'
+        }
       },
       fontFamily: {
-        sans: ["Poppins", "sans-serif"],
-      },
-    },
+        sans: ['Poppins', 'sans-serif'],
+        jost: ['Jost', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
